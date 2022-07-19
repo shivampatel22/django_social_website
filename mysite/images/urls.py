d655088304ce14bc09016240d6af21bf76dc57b2
shivampatel22 <patelshivam221997@gmail.com>
2022-07-19 20:36:39 +0530

    added view for image likes

--- mysite/images/urls.py
+++ mysite/images/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>', views.image_detail, name='detail'),
+    path('like/', views.image_like, name='like'),
 ]
