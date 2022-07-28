fbaa57af9262c27259ac2edb472dac46d4acdb5f
shivampatel22 <patelshivam221997@gmail.com>
2022-07-28 08:44:05 +0530

    implemented AJAX for Follow/Unfollow

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_details, name='user_detail'),
 ]
