37c194aa952960a7aefcfad54862f22424c64919
shivampatel22 <patelshivam221997@gmail.com>
2022-10-09 17:18:31 +0530

    code refactoring

--- mysite/images/models.py
+++ mysite/images/models.py
@@ -7,12 +7,18 @@ class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField()
+    url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
-    created_at = models.DateField(auto_now_add=True, db_index=True)
+    created_at = models.DateField(auto_now_add=True)
 
     users_liked = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created_at']),
+        ]
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
