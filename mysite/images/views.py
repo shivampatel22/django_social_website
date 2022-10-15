5c25d4f13672bafd5d99a8ebad0ddd5e1b1e6eb2
shivampatel22 <patelshivam221997@gmail.com>
2022-10-15 11:14:52 +0530

    implemented total views using redis

--- mysite/images/views.py
+++ mysite/images/views.py
@@ -8,6 +8,13 @@ from django.http import HttpResponse, JsonResponse
 from common.decorators import ajax_required
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
+
 
 @login_required
 def image_create(request):
@@ -33,7 +40,13 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'image':image, 'section':'images', })
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 
+                  'images/image/detail.html', 
+                  {'image':image, 
+                   'section':'images',
+                   'total_views':total_views})
 
 # @ajax_required
 @login_required
